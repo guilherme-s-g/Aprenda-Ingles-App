bb2466bb71f40934d2d41505582602eb14945756
Guilherme Sabino <59611598+guilhermesabinoqueiroz@users.noreply.github.com>
2022-09-16 19:16:34 -0300

    App aprenda inglês
    Aplicando os conhecimentos adquiridos nas aulas na construção desse app.

---
+++ app/src/main/java/com/example/aprendaingles/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.aprendaingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.aprendaingles.fragment.BichosFragment;
+import com.example.aprendaingles.fragment.NumerosFragment;
+import com.example.aprendaingles.fragment.VogaisFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity
+{
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Bichos", BichosFragment.class)
+                .add("Numeros", NumerosFragment.class)
+                .add("Vogais", VogaisFragment.class)
+                .create());
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        smartTabLayout.setViewPager(viewPager);
+
+    }
+}
---
+++ app/src/main/java/com/example/aprendaingles/fragment/BichosFragment.java
@@ -0,0 +1,148 @@
+package com.example.aprendaingles.fragment;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.aprendaingles.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BichosFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BichosFragment extends Fragment implements View.OnClickListener
+{
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private ImageView imageCao, imageGato, imageLeao, imageMacaco, imageOvelha, imageVaca;
+    private MediaPlayer mediaPlayer;
+
+    public BichosFragment()
+    {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BichosFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BichosFragment newInstance(String param1, String param2)
+    {
+        BichosFragment fragment = new BichosFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null)
+        {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_bichos, container,false);
+
+        imageCao = view.findViewById(R.id.imageCao);
+        imageGato = view.findViewById(R.id.imageGato);
+        imageLeao = view.findViewById(R.id.imageLeao);
+        imageMacaco = view.findViewById(R.id.imageMacaco);
+        imageOvelha = view.findViewById(R.id.imageOvelha);
+        imageVaca = view.findViewById(R.id.imageVaca);
+
+        imageCao.setOnClickListener(this);
+        imageGato.setOnClickListener(this);
+        imageLeao.setOnClickListener(this);
+        imageMacaco.setOnClickListener(this);
+        imageOvelha.setOnClickListener(this);
+        imageVaca.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.imageCao:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.dog);
+                tocarSom();
+                break;
+            case R.id.imageGato:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.cat);
+                tocarSom();
+                break;
+            case R.id.imageLeao:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.lion);
+                tocarSom();
+                break;
+            case R.id.imageMacaco:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.monkey);
+                tocarSom();
+                break;
+            case R.id.imageOvelha:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.sheep);
+                tocarSom();
+                break;
+            case R.id.imageVaca:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.cow);
+                tocarSom();
+                break;
+        }
+    }
+
+    public void tocarSom()
+    {
+        if(mediaPlayer != null)
+        {
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+            {
+                @Override
+                public void onCompletion(MediaPlayer mp)
+                {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if (mediaPlayer !=null)
+        {
+            mediaPlayer.release();
+        }
+    }
+}
